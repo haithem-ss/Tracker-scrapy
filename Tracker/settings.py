13d5b51a2991f53c018379f58b700066260768b1
haithemss <haithemhadji@gmail.com>
2022-07-29 22:32:56 +0100

    bypass

--- Tracker/settings.py
+++ Tracker/settings.py
@@ -86,3 +86,10 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.retry.RetryMiddleware': 90,
+    'tutorial.randomproxy.RandomProxy': 100,
+    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' : None,
+    'tutorial.spiders.rotate_useragent.RotateUserAgentMiddleware' :400,    
+}
